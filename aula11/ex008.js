bfe416d2615d5c6a2834d5d252651fabb25d845c
regispicaz <registavares@protonmail.com>
2021-09-07 18:13:05 -0300

    Adição de Exercícios

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 80.5
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+if(vel > 60){//Condição simples
+    console.log(`Você ultrapassou a velocidade máxima permitida, MULTADO!`)
+}
+console.log(`Dirija sempre usando sinto de segurança!`)
---
+++ aula12/ex011.js
@@ -0,0 +1,8 @@
+var idade = 66
+if (idade < 16){
+    console.log(`Você tem ${idade} anos, portanto não pode votar"`)
+}else if(idade <18 || idade > 65){
+        console.log(`Você tem ${idade} e seu voto é opcional!`)
+    }else{
+        console.log(`Você tem ${idade } anos o seu Voto é Obrigatório!`)
+}
