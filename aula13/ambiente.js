191f02c3cf14033dd946468ed931280da767b116
Régis Picáz Tavares <registavares@protonmail.com>
2021-09-28 22:16:38 -0300

    Repetição "for"

---
+++ aula13/ambiente.js
@@ -0,0 +1,18 @@
+var cont = 1 //Variável 
+
+//Estrutura de repetição com teste lógico no final do while('Será executado enquento a  condição for verdadeira')
+do{
+    console.log(`Passo ${cont}`)
+    cont++
+}
+while (cont <= 6)
+
+/*
+var cont = 1 //Variável
+
+ //Estrutura de repetição com teste lógico no início while ('Será executado enquento a  condição for verdadeira')
+while (cont <= 6){
+    console.log(`Passo ${cont}`)
+    cont++
+}
+*/
